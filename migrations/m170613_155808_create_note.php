3b10148f2044cd7db04755ecd14f61a9076d16b8
anr81 <f1_anr@mail.ru>
2017-06-14 12:52:15 +0300

    initial commit

---
+++ migrations/m170613_155808_create_note.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m170613_155808_create_note extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170613_155808_create_note cannot be reverted.\n";
+
+        return false;
+    }
+
+    
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        
+    }
+
+    public function down()
+    {
+        echo "m170613_155808_create_note cannot be reverted.\n";
+
+        return false;
+    }
+    
+}
